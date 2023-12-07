ef2d78e182ada37e5fa262a374091c659d29cb43
ASwathy <ec2-user@ip-172-31-39-221.ap-south-1.compute.internal>
2023-12-07 13:29:42 +0000

    new-sg

--- main.tf
+++ main.tf
@@ -49,3 +49,27 @@ resource "aws_security_group" "frontend2" {
   }
 }
 
+
+
+
+
+
+resource "aws_security_group" "new_sg" {
+  name        = "${var.project_name}-${var.project_env}-new_sg"
+  description = "${var.project_name}-${var.project_env}-new_sg"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
